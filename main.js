9f5a4ab14f2c0788ec7785c9f08bf08d3c4d1515
jordana <133382207+jordana007@users.noreply.github.com>
2024-09-20 12:12:06 -0300

    20/09/24

---
+++ main.js
@@ -0,0 +1,12 @@
+alert("Oi Jojo");
+console.log("Oi Lol");
+
+var titulo = document.querySelector("h1");
+titulo.textContent = "Jordana Nutrição";
+
+var trPaulo = document.querySelector("#paulo");
+
+console.log(trPaulo);
+
+var tdPeso = trPaulo.querySelector(".info-peso");
+tdPeso.textContent = "150";
