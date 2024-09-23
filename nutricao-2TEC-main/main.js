5b616a5d00512c50c311d71b4073417b4895d2de
jordana <133382207+jordana007@users.noreply.github.com>
2024-09-23 10:37:49 -0300

    23/09/2024

---
+++ nutricao-2TEC-main/main.js
@@ -0,0 +1,13 @@
+alert("Oi Jojo");
+console.log("Oi Lol");
+
+var titulo = document.querySelector(".titulo");
+titulo.textContent = "Jordana Nutrição";
+
+var paciente = document.querySelector("#primeiro-paciente");
+
+var tdPeso = paciente.querySelector(".info-peso");
+var peso = tdPeso.textContent;
+
+var tdAltura = paciente.querySelector(".info-altura");
+var altura = tdAltura.textContent;
